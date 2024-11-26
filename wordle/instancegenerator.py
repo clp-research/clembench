0a27b9535dabb0a43698ccf67aab2b8abcfb9c5e
Gnurro <knawurzelkopp@hotmail.de>
2024-11-26 18:59:36 +0100

    Consolidate variants into core wordle: Variant instances are now generated into the main instances file as additional experiments; Change config.json to hold variant configs under variant name keys

--- wordle/instancegenerator.py
+++ wordle/instancegenerator.py
@@ -1,13 +1,9 @@
-"""
-Generate instances for the taboo game.
-
+"""Generate instances for the wordle game.
+All three variants' instances will be created as different experiments, with both difficulty levels each.
 Creates files in ./instances
 """
-import random
 import logging
 import os
-
-from tqdm import tqdm
 
 from clemcore.clemgame import GameInstanceGenerator
 
@@ -35,35 +31,37 @@ class WordleGameInstanceGenerator(GameInstanceGenerator):
             Only update their values with the text in the new language
         """
         # The relative path is used to force looking for the file in the wordle directory
-        lang_keywords = self.load_json("../wordle/resources/langconfig.json")
+        lang_keywords = self.load_json("resources/langconfig")
         self.lang_keywords = lang_keywords[language]    
 
     def on_generate(self):
         self.experiment_config = self.load_json("resources/config.json")
-        # TODO: consolidate variants by having them all in config.json -> just different experiments, not different games
         # TODO: check if game registry can be used to handle variants?
         self._setresponseformatkeywords(LANGUAGE)
 
-        self.instance_utils = InstanceUtils(
-            os.path.dirname(os.path.abspath(__file__)),
-            self.experiment_config,
-            self.game_name,
-            LANGUAGE)
+        for variant, variant_config in self.experiment_config.items():
+            print(f"Wordle variant '{variant}' config: {variant_config}")
 
-        target_words_test_dict = self.instance_utils.select_target_words(SEED)
+            self.instance_utils = InstanceUtils(
+                os.path.dirname(os.path.abspath(__file__)),
+                variant_config,
+                variant,
+                LANGUAGE)
 
-        word_difficulty = list(target_words_test_dict.keys())
+            target_words_test_dict = self.instance_utils.select_target_words(SEED)
 
-        for difficulty in word_difficulty:
-            experiment_name = f'{difficulty}_words_{self.experiment_config["name"]}'
-            experiment = self.add_experiment(experiment_name)
-            self.instance_utils.update_experiment_dict(experiment, self.lang_keywords)
+            word_difficulty = list(target_words_test_dict.keys())
 
-            for index, word in enumerate(target_words_test_dict[difficulty]):
-                game_instance = self.add_game_instance(experiment, index + 1)
-                self.instance_utils.update_game_instance_dict(
-                    game_instance, word, difficulty
-                )
+            for difficulty in word_difficulty:
+                experiment_name = f'{difficulty}_words_{variant_config["name"]}'
+                experiment = self.add_experiment(experiment_name)
+                self.instance_utils.update_experiment_dict(experiment, self.lang_keywords)
+
+                for index, word in enumerate(target_words_test_dict[difficulty]):
+                    game_instance = self.add_game_instance(experiment, index + 1)
+                    self.instance_utils.update_game_instance_dict(
+                        game_instance, word, difficulty
+                    )
 
 if __name__ == "__main__":
     WordleGameInstanceGenerator(GAME_NAME).generate()                
--- wordle/utils/instance_utils.py
+++ wordle/utils/instance_utils.py
@@ -142,7 +142,6 @@ class InstanceUtils(GameResourceLocator):
         return data
 
     def read_word_lists(self):
-        print(f"InstanceUtils.read_word_lists() called")
         official_words = []
         # officially recognized wordle words are downloaded from
         # https://github.com/3b1b/videos/blob/master/_2022/wordle/data/allowed_words.txt

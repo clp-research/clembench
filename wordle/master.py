a6c4a55062545b9f0aaa1e7c14c0c3fa0e349b06
kranti <kranti.chalamalasetti@uni-potsdam.de>
2025-02-20 17:30:07 +0100

    wordle: added ContextExceededError handling

--- wordle/master.py
+++ wordle/master.py
@@ -5,7 +5,7 @@ import re
 import copy
 import numpy as np
 
-from clemcore.backends import Model, HumanModel
+from clemcore.backends import Model, HumanModel, ContextExceededError
 from clemcore.clemgame import GameMaster, GameBenchmark, GameScorer, GameSpec
 import clemcore.clemgame.metrics as metrics
 
@@ -682,7 +682,15 @@ class WordleGameMaster(GameMaster):
             self.log_event(from_="GM", to=use_from, action=action)
 
         # make an API call (or get a programmatic response) from player a
-        prompt, raw_answer, answer = use_player(use_player.history, self.current_turn)
+        try:
+            prompt, raw_answer, answer = use_player(use_player.history, self.current_turn)
+        except ContextExceededError as error:
+            logger.error(f"Current Turn: {self.current_turn}, Error in response from player {player}: {error}")
+            self.aborted = True
+            prompt = use_player.history
+            raw_answer = "Context token limit exceeded"
+            answer = "Context token limit exceeded"
+            #return None
         # add API call to the records
         action = {"type": "get message", "content": answer}
         self.log_event(

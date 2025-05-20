a58546535418e8c828e82fc9531919e7b5d1a278
Philipp Sadler <philipp.sadler@gmail.com>
2025-05-20 15:01:53 +0200

    refactor player logging for referencegame

--- referencegame/master.py
+++ referencegame/master.py
@@ -62,16 +62,15 @@ class ReferenceGameMaster(GameMaster):
     def setup(self, **game_instance):
         self.game = ReferenceGame(game_instance)
         self.instruction_giver = InstructionGiver(self.player_models[0],
-                                                  name="Player 1 (InstructionGiver)",
+                                                  name="Player 1",
+                                                  game_role="Instruction Giver",
                                                   game_recorder=self.game_recorder)
         self.instruction_follower = InstructionFollower(self.player_models[1],
-                                                        name="Player 2 (InstructionFollower)",
+                                                        name="Player 2",
+                                                        game_role="Instruction Follower",
                                                         game_recorder=self.game_recorder)
-        self.log_players({
-            "GM": "Game master for referencegame",
-            "Player_1": self.player_models[0].get_name(),
-            "Player_2": self.player_models[1].get_name()}
-        )
+        self.log_player(self.instruction_giver)
+        self.log_player(self.instruction_follower)
 
     def play(self) -> None:
         self.turn()

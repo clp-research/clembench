ed4836f5bbeff21247502ff92ef013e7f2ab2671
Jonathan Jordan <knawurzelkopp@hotmail.de>
2025-06-03 10:55:03 +0200

    Adapt hellogame master.py to clemcore 3.1.0 (#45)
    * Adapt hellogame master.py to clemcore 3.1.0
    
    * Change hellogame Greeter to use prototype ReasoningPlayer class for CoT recording

--- hellogame/master.py
+++ hellogame/master.py
@@ -2,7 +2,7 @@ import string
 from typing import Dict, List
 
 from clemcore.backends import Model, CustomResponseModel
-from clemcore.clemgame import GameBenchmark, Player, DialogueGameMaster, GameSpec, GameRecorder
+from clemcore.clemgame import GameBenchmark, Player, ReasoningPlayer, DialogueGameMaster, GameSpec, GameRecorder
 import logging
 
 logger = logging.getLogger(__name__)
@@ -18,7 +18,7 @@ class Greeted(Player):
         return f"{self.target_name}: Hi, thanks for having me!"
 
 
-class Greeter(Player):
+class Greeter(ReasoningPlayer):
 
     def __init__(self, model: Model):
         super().__init__(model)
@@ -70,7 +70,7 @@ class HelloGame(DialogueGameMaster):
             self.log_to_self("greeting failed", f"missing words=[{','.join(self.missing_words)}]")
         return False
 
-    def _validate_player_response(self, player: Player, utterance: str) -> bool:
+    def _parse_response(self, player: Player, utterance: str) -> bool:
         # Check responses for specific players
         if player == self.greeter:
             # Check rule: utterance starts with key word
@@ -85,11 +85,17 @@ class HelloGame(DialogueGameMaster):
                 if required_word not in utterance:
                     self.success = False
                     self.missing_words.append(required_word)
-        return True
+        return utterance, True
 
-    def _on_valid_player_response(self, player: Player, parsed_response: str):
+    def _advance_game(self, player: Player, parsed_response: str):
         if player == self.greeter:
             self.set_context_for(self.greeted, parsed_response)
+
+    def compute_turn_score(self):
+        score = 0
+        if self.success:
+            score = 1
+        return score
 
     def compute_episode_score(self):
         score = 0

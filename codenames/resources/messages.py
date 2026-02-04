99c938a0833cc42bb370f8b7a79b547fd4725d8e
Eszter Bukovszky <eszter.bukovszky@uni-potsdam.de>
2026-02-04 20:45:50 +0100

    added messages.py and prefixes.py

---
+++ codenames/resources/messages.py
@@ -0,0 +1,32 @@
+MESSAGES = {
+    "en": {
+        "teammate_turn_ended": "Your teammate's turn ended there.",
+        "word_correct": "The word {word} was guessed correctly.",
+        "words_correct": "The words {words} were guessed correctly.",
+        "word_incorrect": "The word {word} was guessed but is an {assignment} word.",
+        "your_turn_ended": "Your turn ended there."
+    },
+    "hu": {
+        "teammate_turn_ended": "Itt ért véget a csapattársad köre.",
+        "word_correct": "A(z) {word} szót helyesen tippelték.",
+        "words_correct": "A(z) {words} szavakat helyesen tippelték.",
+        "word_incorrect": "A(z) {word} szót tippelték, de ez egy {assignment} szó.",
+        "your_turn_ended": "Itt ért véget a köröd."
+    }
+}
+ASSIGNMENT_LABELS = {
+    "en": {
+        "team": "team",
+        "opponent": "opponent",
+        "innocent": "innocent",
+        "assassin": "assassin"
+    },
+    "hu": {
+        "team": "csapat",
+        "opponent": "ellenfél",
+        "innocent": "semleges",
+        "assassin": "gyilkos"
+    }
+}
+
+
---
+++ codenames/resources/prefixes.py
@@ -0,0 +1,12 @@
+PREFIXES = {
+    "en": {
+        "clue": "CLUE: ",
+        "targets": "TARGETS: ",
+        "guess": "GUESS: "
+    },
+    "hu": {
+        "clue": "NYOM: ",
+        "targets": "CÉLSZAVAK: ",
+        "guess": "TIPP: "
+    }
+}
